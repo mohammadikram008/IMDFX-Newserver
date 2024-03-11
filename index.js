4acc0efd664f3f666df454133f6070c219912c22
@codewithDevWebPro <115881946+Moeen-Kazmi369@users.noreply.github.com>
2024-03-11 16:18:30 +0800

    sad

--- index.js
+++ index.js
@@ -17,31 +17,30 @@ app.get('/', async (req, res) => {
   res.send("heloo")
 
 });
-app.post('/', async (req, res) => {
-  res.send("heloo login")
-  //     try {
-  //       const { email, password } = req.body;
-  //       // console.log(" email, password", email, password)
-  //       const user = await User.findOne({ email }).exec();
+app.post('/login', async (req, res) => {
+      try {
+        const { email, password } = req.body;
+        // console.log(" email, password", email, password)
+        const user = await User.findOne({ email }).exec();
 
-  //       if (!user) {
-  //         // return res.status(404).json('User not found');
-  // console.log("User not found");
-  //       }
+        if (!user) {
+  console.log("User not found");
+          return res.status(404).json('User not found');
+        }
 
-  //       if (user.password !== password) {
-  //         return res.status(401).json('Invalid password');
-  //       }
-  //       // const secretKey = generateSecretKey();
-  //       // console.log(secretKey);
+        if (user.password !== password) {
+          return res.status(401).json('Invalid password');
+        }
+        // const secretKey = generateSecretKey();
+        // console.log(secretKey);
 
-  //       // const token = jwt.sign({ email: user._id }, secretKey);
-  //       // console.log(token);
-  //       res.status(200).json(user._id);
-  //     } catch (error) {
-  //       res.status(500).json('Error finding user');
-  //       console.log("error",error);
-  //     }
+        // const token = jwt.sign({ email: user._id }, secretKey);
+        // console.log(token);
+        res.status(200).json(user._id);
+      } catch (error) {
+        res.status(500).json('Error finding user');
+        console.log("error",error);
+      }
 });
 const router = require('./Routes/RouteLogins/User')
 app.use('/api', router);
